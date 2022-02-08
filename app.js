ef0ef045d4b9e5b46e59363a82c5237a8ffcd7b0
aziz <azizalghanim98@gmail.com>
2022-02-08 20:46:38 +0300

    task 7,8

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const connectDb = require('./database');
-const productsRoutes = require('./api/products/routes');
+const express = require("express");
+const cors = require("cors");
+const connectDb = require("./database");
+const productsRoutes = require("./api/products/routes");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 connectDb();
 
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(req.method, req.protocol, req.hostname, req.path);
+});
+
 // Routes
-app.use('/products', productsRoutes);
+app.use("/products", productsRoutes);
 
 app.listen(process.env.PORT || 5000);
